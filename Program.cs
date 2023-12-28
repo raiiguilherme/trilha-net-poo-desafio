4e5a6389259213c92fe1d0b822d18f13856cff33
raiiguilherme <raiguilherme322@gmail.com>
2023-12-27 21:47:34 -0300

    PROJETO FINALIZADO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Celular Nokia");
+Nokia n = new Nokia("1234", "winphone", "1321321312", 8);
+n.Ligar();
+n.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Iphone i = new Iphone("434223", "Iphone 11", "132135654312", 8);
+i.ReceberLigacao();
+i.InstalarAplicativo("Youtube");
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
